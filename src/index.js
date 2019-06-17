54cd91697f84c8ae9e5e497be88554c26dabc351
alex.chen <pluscda@gmail.com>
2019-06-17 16:41:30 +0800

    3
    3

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import logMessage from './js/logger'
+
+import './css/style.css'
+
+
+
+if (module.hot)       // eslint-disable-line no-undef
+  module.hot.accept() // eslint-disable-line no-undef
---
+++ src/server/server-dev.js
@@ -0,0 +1,61 @@
+import path from 'path'
+import express from 'express'
+import webpack from 'webpack'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
+import config from '../../webpack.dev.config.js'
+import data from '../../src/data/mock-data.json'
+
+const random = (min, max) => Math.floor(Math.random() * (max - min) ) + min;
+const genAdId = () => `${+new Date()}-${random(0, 1000)}`;
+
+const app = express(),
+            DIST_DIR = __dirname,
+            HTML_FILE = path.join(DIST_DIR, 'index.html'),
+            compiler = webpack(config)
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath
+}))
+app.use(webpackHotMiddleware(compiler))
+
+
+//get random ad from mock data
+const getAd = (type = '') => {
+    const ads = type
+      ? data.filter(ad => ad.type === type || !ad.success)
+      : data;
+  
+    const ad = ads[random(0, ads.length)];
+    return {
+      ...ad,
+      id: genAdId()
+    }
+  }
+
+//api endpoint
+app.get('/ads', (req, res) => {
+    /**
+     * type: requested ad type
+     */
+    const { type = '' } = req.query;
+    res.json(getAd(type.toUpperCase()));
+})
+
+app.get('*', (req, res, next) => {
+  compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
+  if (err) {
+    return next(err)
+  }
+  res.set('content-type', 'text/html')
+  res.send(result)
+  res.end()
+  })
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`App listening to ${PORT}....`)
+    console.log('Pres2s Ctrl+C to quit.')
+})
