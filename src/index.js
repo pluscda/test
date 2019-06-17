02cfe8a14bf40b61b1172235ae44249ef0d2c467
Alex <pluscda@gmail.com>
2019-06-17 21:25:51 +0800

    refacor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import ajax from './js/adder'
+import adLib from '../lib/advLib'
 import './css/style.css'
+window.alert = () => {};
+const initAdv = () => {
+    const adObj = new adLib({});
+}
 
-ajax();
 
+
+
+
+
+
+
+if (window.addEventListener) {
+    window.addEventListener('load', initAdv);
+} else if (window.attachEvent) {
+    window.attachEvent('onload', initAdv);
+}
 if (module.hot)       // eslint-disable-line no-undef
   module.hot.accept() // eslint-disable-line no-undef
 
