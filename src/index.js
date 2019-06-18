9027f1bdc12e91fefb85035f1b6825ec1faeb458
alex.chen <pluscda@gmail.com>
2019-06-18 11:04:32 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,50 +1,31 @@
-import adLib from '../lib/advLib'
+import AotterPlayer from '../lib/advLib'
 import './css/style.css'
-window.alert = () => {};
+//window.alert = () => {};
 
+let cbAdLoaded = (el) => {
+    console.log('data is loaded ok');
+    // /el.style.transform="scale(0)";
+}
+let cbAdFailed = (data) => {
 
-class AotterConfig {
-    constructor(obj) {
-        let {width, height, id, loadType} = obj;
-        this.width = width;
-        this.height = height;
-        this.id = id;
-        this.loadType = loadType;
-        this.cbAdLoaded = this.cbAdLoaded.bind(this);
-        this.cbAdFailed = this.cbAdFailed.bind(this);
-        this.cbAdImpression = this.cbAdImpression.bind(this);
-    }
-    constructAotterObj(){
-      const obj = {
-            width: this.width,
-            height: this.height,
-            loadType: this.loadType,
-            cbAdLoaded: this.cbAdLoaded,
-            cbAdFailed: this.cbAdFailed,
-            cbAdImpression: this.cbAdImpression,
-      };
-      return obj;
-    }
-    cbAdLoaded (data) {
+}
+let cbAdImpression = (data) => {
 
-    }
-    cbAdFailed (data){
-
-    }
-    cbAdImpression(data){
-
-    }
 }
 
 const initAdv = () => {
-    let aotter1 = new AotterConfig( {
+    let config =  {
        width: 650,
        height: 390,
        id: 'player_aotter1',
        loadType: 'VIDEO',
-    });
-
-    const adObj = new adLib(aotter1.constructAotterObj());
+       events: {
+        cbAdLoaded: cbAdLoaded,
+        cbAdFailed: cbAdFailed,
+        cbAdImpression: cbAdImpression,
+       }
+    };
+    new AotterPlayer(config);
 }//initAdv
 
 if (window.addEventListener) {
